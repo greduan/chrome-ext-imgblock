71fa6fe1dcce07d33e1836cd157bee35f2ba6bde
Greduan <eduanlavaque@gmail.com>
2014-11-09 15:58:05 -0600

    Now we know if we clicked on the right thing

--- contentscript.js
+++ contentscript.js
@@ -16,11 +16,23 @@ function getEventTarget(e) {
     return e.target || e.srcElement;
 }
 
+function isInArray(array, string) {
+    var i = array.indexOf(string);
+    if (i < 0) {
+        return false;
+    } else if (i >= 0) {
+        return true;
+    }
+}
+
 function clickEventFunc(e) {
     var target = getEventTarget(e);
 
-    if (target.tagName.toLowerCase() === 'img') {
-        console.log(target);
+    if (target.tagName.toLowerCase() === 'div'
+        && isInArray(target.className.split(' '), 'imgblock-ontop')) {
+        // TODO: don't assume it's the first child
+        if (isInArray(target.parentElement.firstChild.className.split(' '), 'imgblock-img')) {
+        }
     }
 }
 
