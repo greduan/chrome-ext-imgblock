a1c1f7ae981cc553cd0d668ae704e96205c67f1f
Greduan <eduanlavaque@gmail.com>
2014-11-09 17:26:05 -0600

    Background image and bit of refactoring
    Next comes making the width and height the same as the original,
    otherwise give it a nice default.

--- contentscript.js
+++ contentscript.js
@@ -2,21 +2,26 @@
 
 // the placeholder img stuff
 var imgs = document.getElementsByTagName('img'),
-    i;
+    i,
+    curImg,
+    parent;
 
 for (i = 0; i < imgs.length; i += 1) {
+    curImg = $(imgs[i]),
+    parent = curImg.parent();
+
     // check if the img is immediately surrounded by an anchor
     // if it is then we want to do some stuff with the anchor, not the img
-    if ($(imgs[i]).parent().get(0).tagName.toLowerCase() === 'a') {
-        $(imgs[i]).parent().wrap('<div class="imgblock-container"></div>');
-        $(imgs[i]).parent().addClass('imgblock-onbottom');
-        $(imgs[i]).parent().after('<div class="imgblock-ontop">LOAD</div>');
+    if (parent.get(0).tagName.toLowerCase() === 'a') {
+        parent.wrap('<div class="imgblock-container"></div>');
+        parent.addClass('imgblock-onbottom');
+        parent.after('<div class="imgblock-ontop"></div>');
     } else {
-        $(imgs[i]).wrap('<div class="imgblock-container"></div>');
-        $(imgs[i]).addClass('imgblock-onbottom');
-        $(imgs[i]).after('<div class="imgblock-ontop">LOAD</div>');
+        curImg.wrap('<div class="imgblock-container"></div>');
+        curImg.addClass('imgblock-onbottom');
+        curImg.after('<div class="imgblock-ontop"></div>');
     }
-    $(imgs[i]).addClass('imgblock-img');
+    curImg.addClass('imgblock-img');
 }
 
 // click event stuff
@@ -41,6 +46,7 @@ function clickEventFunc(e) {
         && isStringInArray(target.className.split(' '), 'imgblock-ontop')) {
         // TODO: don't assume it's the first child
         if (isStringInArray(target.parentElement.firstChild.className.split(' '), 'imgblock-img')) {
+            console.log('click yo');
         }
     }
 }
