985e863cc46902ebb4fbdda5e0778a44627e0b88
Greduan <eduanlavaque@gmail.com>
2014-11-11 11:52:46 -0600

    Now saves settings and uses them

--- options.js
+++ options.js
@@ -1,6 +1,16 @@
 ;(function () {
     'use strict';
 
+    var opts = {};
+
+    document.addEventListener('DOMContentLoaded', function () {
+        chrome.storage.sync.get(function (obj) {
+            opts.parentLimit = obj.options.parentLimit || 2;
+
+            document.getElementsByName('parentLimit')[0].value = opts.parentLimit;
+        });
+    });
+
     document.getElementsByName('save')[0].addEventListener('click', function () {
         var parentLimit = document.getElementsByName('parentLimit')[0].value;
 
