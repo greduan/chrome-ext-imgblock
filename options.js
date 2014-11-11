755b550a35d27eaf373884993d92792572f4ea05
Greduan <eduanlavaque@gmail.com>
2014-11-11 14:24:00 -0600

    Added whitelist option

--- options.js
+++ options.js
@@ -6,17 +6,21 @@
     document.addEventListener('DOMContentLoaded', function () {
         chrome.storage.sync.get(function (obj) {
             opts.parentLimit = obj.options.parentLimit || 2;
+            opts.whitelist = obj.options.whitelist || '';
 
             document.getElementsByName('parentLimit')[0].value = opts.parentLimit;
+            document.getElementsByName('whitelist')[0].value = opts.whitelist;
         });
     });
 
     document.getElementsByName('save')[0].addEventListener('click', function () {
-        var parentLimit = document.getElementsByName('parentLimit')[0].value;
+        var parentLimit = document.getElementsByName('parentLimit')[0].value,
+            whitelist = document.getElementsByName('whitelist')[0].value;
 
         chrome.storage.sync.set({
             options: {
-                parentLimit: parentLimit
+                parentLimit: parentLimit,
+                whitelist: whitelist
             }
         }, function () {
             document.getElementById('saved').style.display = 'block';
